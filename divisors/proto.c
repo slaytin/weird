f11b581f93b56bdcb8221f1fe80de687d063238a
Juan Ann <vshoosv@gmail.com>
2019-11-15 19:16:27 -0600

    secondary method for input added in divisors dir

--- divisors/proto.c
+++ divisors/proto.c
@@ -1,24 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// early vers of program to search for weird numbers - 
+// 						https://en.wikipedia.org/wiki/Weird_number
 
-int main(int argc, char *argv[]) 
+// so far:  takes 1 argument, returns its divisors excluding itself
+
+int main() 
 {
-	unsigned long i, num;
-	if (argc < 2) {
-		printf("Enter one number\n");
-	}
-	num = strtol(argv[1], NULL, 10);
-
-//	int v = 4;
+	unsigned long i, num, total;
+//	if (argc < 2) {
+//		printf("Enter one number\n");
+//	}
 	
-	for (i = num / 2; i > 0; i--) {
-		
-		
-		
-		if (num % i == 0)
-			printf("%lu\n", i);
-				
+	num = 0;
+	while (num != 1) {
+		printf("Enter a number(1 to exit): ");
+		scanf("%lu", &num);
+//	num = strtol(argv[1], NULL, 10);
+		total = 0;
+	
+		for (i = num / 2; i > 0; --i) {		// starts at half, less nos to check
+			if (num % i == 0){             // checks if divisor
+				printf("%lu\n", i);
+				total += i;	
+			}	// Sums the divisors
+		}
+	
+		printf("Sum of divisors = %lu\n", total);
 	}
-//	void assert(v % 2 == 0);
 }
